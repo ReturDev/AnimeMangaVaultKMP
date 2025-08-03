d7592f24f603d4b43d54ec8cf2f0633544bf7477
sergiomm50 <sergiomm50@gmail.com>
2025-08-03 23:29:14 +0200

    refactor: replace genre and demographic IDs with full GenreModel objects in VisualMediaModel
    - Replaced abstract properties genreIds and demographicIds with genres and demographics of type List<GenreModel>
    - Improves type safety and encapsulation of genre-related data

--- composeApp/src/commonMain/kotlin/com/returdev/animemanga/domain/model/visualmedia/VisualMediaModel.kt
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/domain/model/visualmedia/VisualMediaModel.kt
@@ -1,18 +1,20 @@
 package com.returdev.animemanga.domain.model.visualmedia
 
+import com.returdev.animemanga.domain.model.core.GenreModel
 import com.returdev.animemanga.domain.model.core.TitleModel
 
+
 /**
- * Abstract class representing a visual media item with its main attributes.
+ * Abstract class representing a visual media entity with common attributes.
  *
- * @property basicInfo Basic information about the visual media (such as id, images, title, type, score).
- * @property extraTitles Additional titles for the media (e.g., in different languages or synonyms).
- * @property numberOfScorers The number of users who have scored/rated the media.
- * @property rank The ranking position of the media.
- * @property synopsis A brief summary or description of the media.
- * @property status The current status of the media (e.g., ongoing, completed).
- * @property genreIds List of genre identifiers associated with the media.
- * @property demographicIds List of demographic identifiers associated with the media.
+ * @property basicInfo Basic information about the visual media (e.g., id, images, title, type, score).
+ * @property extraTitles Additional titles for the media (such as in different languages or synonyms).
+ * @property numberOfScorers Number of users who have rated the media.
+ * @property rank Ranking position of the media.
+ * @property synopsis Brief summary or description of the media.
+ * @property status Current status of the media (e.g., ongoing, completed).
+ * @property demographics List of demographic genres associated with the media.
+ * @property genres List of genres associated with the media.
  */
 abstract class VisualMediaModel {
     abstract val basicInfo : VisualMediaBasicModel
@@ -21,6 +23,6 @@ abstract class VisualMediaModel {
     abstract val rank: Int
     abstract val synopsis: String
     abstract val status: String
-    abstract val genreIds: List<Int>
-    abstract val demographicIds: List<Int>
+    abstract val demographics : List<GenreModel>
+    abstract val genres : List<GenreModel>
 }
