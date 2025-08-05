fc2113b4b8558c565bc41c5e5a98f687f67146d9
sergiomm50 <sergiomm50@gmail.com>
2025-08-05 23:46:09 +0200

    feat: add AnimeStatusFilters enum for filtering anime by airing status
    - Added enum AnimeStatusFilters with values: AIRING, COMPLETE, UPCOMING
    - Enables filtering anime search results based on their current status

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/domain/model/core/search/anime/AnimeStatusFilters.kt
@@ -0,0 +1,17 @@
+package com.returdev.animemanga.domain.model.core.search.anime
+
+/**
+ * Enum class representing the available status filters for anime search results.
+ *
+ * The filters include:
+ * - AIRING: Anime that are currently airing.
+ * - COMPLETE: Anime that have finished airing.
+ * - UPCOMING: Anime that are scheduled to air in the future.
+ */
+enum class AnimeStatusFilters {
+
+    AIRING,
+    COMPLETE,
+    UPCOMING;
+
+}
