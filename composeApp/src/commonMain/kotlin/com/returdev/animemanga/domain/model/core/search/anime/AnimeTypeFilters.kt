4b5be1306954c87566960e9b7bdffddef46c30b7
sergiomm50 <sergiomm50@gmail.com>
2025-08-05 23:46:56 +0200

    feat: add AnimeTypeFilters enum for filtering anime by media type
    - Introduced AnimeTypeFilters enum with values: TV, MOVIE, OVA, SPECIAL, ONA, MUSIC, CM, PV, TV_SPECIAL
    - Facilitates filtering anime search results based on their format/type

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/domain/model/core/search/anime/AnimeTypeFilters.kt
@@ -0,0 +1,29 @@
+package com.returdev.animemanga.domain.model.core.search.anime
+
+/**
+ * Enum class representing the available type filters for anime search results.
+ *
+ * The filters include:
+ * - TV: Television series.
+ * - MOVIE: Anime movies.
+ * - OVA: Original Video Animation.
+ * - SPECIAL: Special episodes or releases.
+ * - ONA: Original Net Animation.
+ * - MUSIC: Music-related anime.
+ * - CM: Commercials.
+ * - PV: Promotional videos.
+ * - TV_SPECIAL: TV specials.
+ */
+enum class AnimeTypeFilters {
+
+    TV,
+    MOVIE,
+    OVA,
+    SPECIAL,
+    ONA,
+    MUSIC,
+    CM,
+    PV,
+    TV_SPECIAL;
+
+}
