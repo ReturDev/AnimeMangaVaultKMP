ebafc04e8c0c5ebb937671706deb21e97f0d978b
sergiomm50 <sergiomm50@gmail.com>
2025-08-05 23:47:37 +0200

    feat: add SortDirection enum for specifying sort order in search results
    - Introduced SortDirection enum with ASCENDANT and DESCENDANT options
    - Enables explicit control over sorting direction in search queries

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/domain/model/core/search/common/SortDirection.kt
@@ -0,0 +1,15 @@
+package github.returdev.animemangavault.core.model.core.filters.common
+
+/**
+ * Enum class representing the possible sort directions for search results.
+ *
+ * The options include:
+ * - ASCENDANT: Sort in ascending order.
+ * - DESCENDANT: Sort in descending order.
+ */
+enum class SortDirection {
+
+    ASCENDANT,
+    DESCENDANT
+
+}
