563947ff247a92772736ea925cb5ea580464c3a6
sergiomm50 <sergiomm50@gmail.com>
2025-08-10 21:13:39 +0200

    feat: add MangaReducedResponse data class for simplified manga API response
    - Includes fields for id, images, title, optional type, and score.

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/data/remote/model/manga/MangaReducedResponse.kt
@@ -0,0 +1,23 @@
+package com.returdev.animemanga.data.remote.model.manga
+
+import com.returdev.animemanga.data.remote.model.core.component.ImageDataComponentResponse
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Data class representing a reduced version of an manga response.
+ *
+ * @property id The unique identifier of the manga (MyAnimeList ID).
+ * @property images The image data associated with the manga.
+ * @property title The title of the manga.
+ * @property type The type of the manga (e.g., Manga, Novel, etc.), nullable.
+ * @property score The score or rating of the manga.
+ */
+@Serializable
+data class MangaReducedResponse(
+    @SerialName("mal_id") val id : Int,
+    @SerialName("images") val images : ImageDataComponentResponse,
+    @SerialName("title") val title : String,
+    @SerialName("type") val type : String?,
+    @SerialName("score") val score : Float
+)
