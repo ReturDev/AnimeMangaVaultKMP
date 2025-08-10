bec085b29d2cd1161f080f871a80dc081a1b13f9
sergiomm50 <sergiomm50@gmail.com>
2025-08-10 17:44:00 +0200

    feat: register platform-specific CacheDataBase provider in Koin module for Android and iOS
    - Implemented actual fun platformModule() to provide getDatabase() via Koin singleOf
    - Enables dependency injection of CacheDataBase in shared and platform-specific code

--- composeApp/src/androidMain/kotlin/com/returdev/animemanga/di/PlatformModule.android.kt
+++ composeApp/src/androidMain/kotlin/com/returdev/animemanga/di/PlatformModule.android.kt
@@ -1,8 +1,10 @@
 package com.returdev.animemanga.di
 
+import com.returdev.animemanga.data.cache.getDatabase
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 actual fun platformModule() : Module = module {
-
+    singleOf(::getDatabase)
 }
