eae0b6b96bc5ee3d531b4f983c384878360fcd1c
sergiomm50 <sergiomm50@gmail.com>
2025-09-10 23:47:14 +0200

    feat: add Type.kt with custom Typography configuration

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/ui/theme/Type.kt
@@ -0,0 +1,13 @@
+package com.returdev.animemanga.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    labelSmall = TextStyle(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Light
+    )
+)
