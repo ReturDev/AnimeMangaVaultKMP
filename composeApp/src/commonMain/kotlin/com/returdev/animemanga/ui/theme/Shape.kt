4869a0ff8417c8196a2f72ac14e2c8bcfa57fe04
sergiomm50 <sergiomm50@gmail.com>
2025-09-10 23:44:47 +0200

    feat: add Shape.kt with BasicVisualMediaItemShape definition

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/ui/theme/Shape.kt
@@ -0,0 +1,6 @@
+package com.returdev.animemanga.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+val BasicVisualMediaItemShape = RoundedCornerShape(8.dp)
