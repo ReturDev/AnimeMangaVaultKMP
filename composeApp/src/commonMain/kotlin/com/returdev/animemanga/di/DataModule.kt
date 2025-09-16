258db5dd0b6c221e634f5450b0400d814b68fffb
sergiomm50 <sergiomm50@gmail.com>
2025-09-16 15:30:42 +0200

    feat: expand data module with repositories, paging sources, and cache/data source factories
    - Add `CoroutineDispatcher` binding with `Dispatchers.IO`.
    - Register repositories: `AnimeRepository`, `MangaRepository`.
    - Register remote repositories: `AnimeRemoteRepository`, `MangaRemoteRepository`.
    - Register paging sources: `AnimePagingSource`, `MangaPagingSource`.
    - Add `cacheDaoFactory` for cache DAOs (seasons, metadata, year, genres).
    - Add `dataSourceFactory` for cache data sources (anime/manga genres, metadata, seasons).

--- composeApp/src/commonMain/kotlin/com/returdev/animemanga/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/di/DataModule.kt
@@ -1,19 +1,30 @@
 package com.returdev.animemanga.di
 
+import com.returdev.animemanga.data.cache.CacheDataBase
 import com.returdev.animemanga.data.cache.datasource.CacheMetadataDataSource
 import com.returdev.animemanga.data.cache.datasource.SeasonCacheDataSource
 import com.returdev.animemanga.data.cache.datasource.genre.AnimeGenreCacheDataSource
 import com.returdev.animemanga.data.cache.datasource.genre.MangaGenreCacheDataSource
+import com.returdev.animemanga.data.paging.AnimePagingSource
+import com.returdev.animemanga.data.paging.MangaPagingSource
 import com.returdev.animemanga.data.remote.core.util.ApiRequestManager
 import com.returdev.animemanga.data.remote.core.util.ApiRequestManagerImpl
+import com.returdev.animemanga.data.remote.repository.AnimeRemoteRepository
+import com.returdev.animemanga.data.remote.repository.MangaRemoteRepository
 import com.returdev.animemanga.data.remote.service.ApiService
+import com.returdev.animemanga.data.repository.AnimeRepository
+import com.returdev.animemanga.data.repository.MangaRepository
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.http.URLProtocol
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.serialization.json.Json
+import org.koin.core.module.Module
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
@@ -31,10 +42,36 @@ val datamodule = module {
             }
         }
     }
-    factoryOf(::ApiService)
+
     factory<ApiRequestManager>{ ApiRequestManagerImpl() }
+    factory<CoroutineDispatcher>{ Dispatchers.IO }
+
+    factoryOf(::ApiService)
+    factoryOf(::AnimeRemoteRepository)
+    factoryOf(::MangaRemoteRepository)
+
+    factoryOf(::AnimeRepository)
+    factoryOf(::MangaRepository)
+
+    factoryOf(::AnimePagingSource)
+    factoryOf(::MangaPagingSource)
+
+    cacheDaoFactory()
+    dataSourceFactory()
+
+}
+
+private fun Module.cacheDaoFactory(){
+    factory{ get<CacheDataBase>().seasonsCacheDao()}
+    factory { get<CacheDataBase>().cacheMetadataDao() }
+    factory { get<CacheDataBase>().yearCacheDao() }
+    factory { get<CacheDataBase>().animeGenreCacheDao() }
+    factory { get<CacheDataBase>().mangaGenreCacheDao() }
+}
+
+private fun Module.dataSourceFactory(){
     factoryOf(::AnimeGenreCacheDataSource)
     factoryOf(::MangaGenreCacheDataSource)
-    factoryOf(::CacheMetadataDataSource)
     factoryOf(::SeasonCacheDataSource)
+    factoryOf(::CacheMetadataDataSource)
 }
