00338f0a3105a347249d76d3882daf50e5227c0a
sergiomm50 <sergiomm50@gmail.com>
2025-09-16 19:12:44 +0200

    feat: introduce MediaCategory enum for distinguishing anime and manga

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/ui/model/core/MediaCategory.kt
@@ -0,0 +1,9 @@
+package com.returdev.animemanga.ui.model.core
+
+/**
+ * Represents the two main categories of visual media supported by the app.
+ */
+enum class MediaCategory {
+    ANIME,
+    MANGA
+}
