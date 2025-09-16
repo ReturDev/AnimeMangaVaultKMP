446dba9895bb9674e90ede872be429d2fdcf0de8
sergiomm50 <sergiomm50@gmail.com>
2025-09-16 15:31:47 +0200

    feat: add cache model extension mappers for seasons and genres
    - Add `SeasonResponse.toCacheEntity` and `List<SeasonResponse>.toCacheEntityList`.
    - Add mappers to convert `GenreDataComponentResponse` into `AnimeGenreCacheEntity` and `MangaGenreCacheEntity`.
    - Add mappers to convert cached anime/manga genres into domain `GenreModel`.

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/data/cache/model/extension/DataCacheExtension.kt
@@ -0,0 +1,54 @@
+package com.returdev.animemanga.data.cache.model.extension
+
+import com.returdev.animemanga.data.cache.model.entity.SeasonCacheEntity
+import com.returdev.animemanga.data.cache.model.entity.genre.AnimeGenreCacheEntity
+import com.returdev.animemanga.data.cache.model.entity.genre.MangaGenreCacheEntity
+import com.returdev.animemanga.data.remote.model.core.component.GenreDataComponentResponse
+import com.returdev.animemanga.data.remote.model.season.SeasonResponse
+import com.returdev.animemanga.domain.model.core.GenreModel
+
+fun SeasonResponse.toCacheEntity() : SeasonCacheEntity {
+    return SeasonCacheEntity(
+        year = this.year,
+        season = this.seasons.joinToString(",")
+    )
+}
+
+fun List<SeasonResponse>.toCacheEntityList() : List<SeasonCacheEntity> {
+    return this.map { it.toCacheEntity() }
+}
+
+fun List<GenreDataComponentResponse>.toAnimeGenreCacheList() : List<AnimeGenreCacheEntity> {
+    return this.map { genre ->
+        AnimeGenreCacheEntity(
+            id = genre.id,
+            name = genre.name,
+        )
+    }
+}
+fun List<GenreDataComponentResponse>.toMangaGenreCacheList() : List<MangaGenreCacheEntity> {
+    return this.map { genre ->
+        MangaGenreCacheEntity(
+            id = genre.id,
+            name = genre.name,
+        )
+    }
+}
+
+fun List<MangaGenreCacheEntity>.toDomain() : List<GenreModel> {
+    return this.map { genre ->
+        GenreModel(
+            id = genre.id,
+            name = genre.name,
+        )
+    }
+}
+
+fun List<AnimeGenreCacheEntity>.toDomain() : List<GenreModel> {
+    return this.map { genre ->
+        GenreModel(
+            id = genre.id,
+            name = genre.name,
+        )
+    }
+}
