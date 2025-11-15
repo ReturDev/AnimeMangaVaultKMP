d38e1e2c1088cf9522285fe384ec02d7b8e71174
sergiomm50 <sergiomm50@gmail.com>
2025-11-15 22:17:25 +0100

    feat: add AnimeSearchScreen composable with integrated AnimeSearchViewModel
    - Introduces dedicated screen for anime search.
    - Connects SearchScreen with AnimeSearchViewModel.
    - Adds navigation callback wrapper including MediaCategory.ANIME.

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/ui/screen/search/anime/AnimeSearchScreen.kt
@@ -0,0 +1,32 @@
+package com.returdev.animemanga.ui.screen.search.anime
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.returdev.animemanga.ui.model.core.MediaCategory
+import com.returdev.animemanga.ui.screen.search.SearchScreen
+
+/**
+ * A composable screen that allows the user to search for anime.
+ *
+ * This screen uses the [SearchScreen] composable to provide the search functionality,
+ * and the [AnimeSearchViewModel] to handle the business logic.
+ *
+ * @param modifier The modifier to be applied to the screen.
+ * @param navigateToDetailScreen A lambda function to navigate to the detail screen of an anime.
+ */
+@Composable
+fun AnimeSearchScreen(
+    modifier : Modifier = Modifier,
+    navigateToDetailScreen: (Int, MediaCategory ) -> Unit,
+) {
+
+    val viewModel: AnimeSearchViewModel = viewModel()
+
+    SearchScreen(
+        modifier = modifier,
+        navigateToDetailScreen = { id -> navigateToDetailScreen(id, MediaCategory.ANIME) },
+        viewModel = viewModel
+    )
+
+}
