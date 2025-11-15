20b7489de507a4dffdb1246270df6641b2f170fe
sergiomm50 <sergiomm50@gmail.com>
2025-11-15 22:14:03 +0100

    feat: add AnimeSearchViewModel with initial and query-based anime search
    - Implements SearchViewModel for anime using AnimeBasicUi.
    - Adds initial search loading TV-type anime by default.
    - Maps domain models to UI models via toUi().
    - Integrates repository search with PagingData flow transformations.

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/ui/screen/search/anime/AnimeSearchViewModel.kt
@@ -0,0 +1,62 @@
+package com.returdev.animemanga.ui.screen.search.anime
+
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.returdev.animemanga.data.repository.AnimeRepository
+import com.returdev.animemanga.domain.model.core.search.SearchFilters
+import com.returdev.animemanga.domain.model.core.search.anime.AnimeRatingFilters
+import com.returdev.animemanga.domain.model.core.search.anime.AnimeTypeFilters
+import com.returdev.animemanga.ui.model.basic.AnimeBasicUi
+import com.returdev.animemanga.ui.model.extension.toUi
+import com.returdev.animemanga.ui.screen.search.SearchViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+/**
+ * ViewModel for the Anime Search screen.
+ *
+ * This ViewModel is responsible for handling the business logic related to searching for anime.
+ * It extends [SearchViewModel] with [AnimeBasicUi] as the type of item.
+ *
+ * @param dispatcher The coroutine dispatcher for background tasks.
+ * @param animeRepository The repository to fetch anime data from.
+ */
+class AnimeSearchViewModel(
+    dispatcher : CoroutineDispatcher,
+    private val animeRepository : AnimeRepository
+) : SearchViewModel<AnimeBasicUi>(dispatcher) {
+
+
+    /**
+     * Performs the initial search to populate the screen before any user query.
+     *
+     * It fetches a default list of TV anime.
+     *
+     * @return A [Flow] of [PagingData] containing [AnimeBasicUi] items.
+     */
+    override suspend fun initialSearch(): Flow<PagingData<AnimeBasicUi>> {
+        return animeRepository.getAnimeSearch(
+            query = "",
+            filters = SearchFilters.AnimeFilters(
+                AnimeTypeFilters.TV
+            )
+        ).map { paging -> paging.map{it.toUi()}}
+    }
+
+
+    /**
+     * Fetches search data based on the provided query.
+     *
+     * @param query The search term entered by the user.
+     * @return A [Flow] of [PagingData] containing the filtered [AnimeBasicUi] items.
+     */
+    override suspend fun getSearchData(query : String) : Flow<PagingData<AnimeBasicUi>> {
+        return animeRepository.getAnimeSearch(
+            query = query,
+            filters = SearchFilters.AnimeFilters()
+        ).map { paging -> paging.map { it.toUi() } }
+    }
+
+
+}
