2d24dead8352c9fd7ed49182b033650d8ec62ed8
sergiomm50 <sergiomm50@gmail.com>
2025-11-16 01:43:48 +0100

    feat: add MangaSearchViewModel for handling manga search functionality
    - Implements initialSearch with default Manga type filter.
    - Adds getSearchData to perform dynamic manga searches.
    - Maps domain models to UI models using toUi().

---
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/ui/screen/search/manga/MangaSearchViewModel.kt
@@ -0,0 +1,57 @@
+package com.returdev.animemanga.ui.screen.search.manga
+
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.returdev.animemanga.data.repository.MangaRepository
+import com.returdev.animemanga.domain.model.core.search.SearchFilters
+import com.returdev.animemanga.domain.model.core.search.manga.MangaTypeFilters
+import com.returdev.animemanga.ui.model.basic.MangaBasicUi
+import com.returdev.animemanga.ui.model.extension.toUi
+import com.returdev.animemanga.ui.screen.search.SearchViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+/**
+ * ViewModel for the Manga Search screen.
+ *
+ * This ViewModel is responsible for handling the business logic related to searching for manga.
+ * It extends [SearchViewModel] with [MangaBasicUi] as the type of item.
+ *
+ * @param dispatcher The coroutine dispatcher for background tasks.
+ * @param mangaRepository The repository to fetch manga data from.
+ */
+class MangaSearchViewModel(
+    dispatcher : CoroutineDispatcher,
+    private val mangaRepository : MangaRepository
+) : SearchViewModel<MangaBasicUi>(dispatcher) {
+
+    /**
+     * Performs the initial search to populate the screen before any user query.
+     *
+     * It fetches a default list of manga.
+     *
+     * @return A [Flow] of [PagingData] containing [MangaBasicUi] items.
+     */
+    override suspend fun initialSearch(): Flow<PagingData<MangaBasicUi>> {
+        return mangaRepository.getMangaSearch(
+            query = "",
+            filters = SearchFilters.MangaFilters(
+                type = MangaTypeFilters.MANGA
+            )
+        ).map { pagingData -> pagingData.map { it.toUi() }}
+    }
+
+    /**
+     * Fetches search data based on the provided query.
+     *
+     * @param query The search term entered by the user.
+     * @return A [Flow] of [PagingData] containing the filtered [MangaBasicUi] items.
+     */
+    override suspend fun getSearchData(query : String) : Flow<PagingData<MangaBasicUi>> {
+        return mangaRepository.getMangaSearch(
+            query = query,
+            filters = SearchFilters.MangaFilters()
+        ).map { pagingData -> pagingData.map { it.toUi() } }
+    }
+}
