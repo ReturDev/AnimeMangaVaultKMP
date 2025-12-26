dae31e67ce663b41f783690dccadf11da0daa528
sergiomm50 <sergiomm50@gmail.com>
2025-12-26 23:16:26 +0100

    feat: register UI ViewModels in Koin module

--- composeApp/src/commonMain/kotlin/com/returdev/animemanga/di/UiModule.kt
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/di/UiModule.kt
@@ -1,7 +1,19 @@
 package com.returdev.animemanga.di
 
+import com.returdev.animemanga.ui.screen.detail.anime.AnimeDetailViewModel
+import com.returdev.animemanga.ui.screen.home.HomeViewModel
+import com.returdev.animemanga.ui.screen.library.LibraryViewModel
+import com.returdev.animemanga.ui.screen.search.anime.AnimeSearchViewModel
+import com.returdev.animemanga.ui.screen.search.manga.MangaSearchViewModel
+import com.returdev.animemanga.ui.screen.showmore.ShowMoreViewModel
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val uiModule = module {
-
+    factoryOf(::HomeViewModel)
+    factoryOf(::ShowMoreViewModel)
+    factoryOf(::LibraryViewModel)
+    factoryOf(::AnimeSearchViewModel)
+    factoryOf(::MangaSearchViewModel)
+    factoryOf(::AnimeDetailViewModel)
 }
