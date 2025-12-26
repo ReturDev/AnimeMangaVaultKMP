c3f40a1f2227c7fc700b881a36f73b4838263752
sergiomm50 <sergiomm50@gmail.com>
2025-12-26 23:14:53 +0100

    feat: add coroutine scope and library data sources to DataModule
    - Provide shared CoroutineScope with SupervisorJob
    - Register LibraryAnimeDataSource and LibraryMangaDataSource with transaction support
    - Add DAO factories for anime and manga library tables
    - Introduce Room transactionRunner helper

--- composeApp/src/commonMain/kotlin/com/returdev/animemanga/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/returdev/animemanga/di/DataModule.kt
@@ -1,10 +1,16 @@
 package com.returdev.animemanga.di
 
+import androidx.room.RoomDatabase
+import androidx.room.Transactor
+import androidx.room.useReaderConnection
 import com.returdev.animemanga.data.cache.CacheDataBase
 import com.returdev.animemanga.data.cache.datasource.CacheMetadataDataSource
 import com.returdev.animemanga.data.cache.datasource.SeasonCacheDataSource
 import com.returdev.animemanga.data.cache.datasource.genre.AnimeGenreCacheDataSource
 import com.returdev.animemanga.data.cache.datasource.genre.MangaGenreCacheDataSource
+import com.returdev.animemanga.data.library.LibraryDatabase
+import com.returdev.animemanga.data.library.datasource.LibraryAnimeDataSource
+import com.returdev.animemanga.data.library.datasource.LibraryMangaDataSource
 import com.returdev.animemanga.data.paging.AnimePagingSource
 import com.returdev.animemanga.data.paging.MangaPagingSource
 import com.returdev.animemanga.data.remote.core.util.ApiRequestManager
@@ -13,6 +19,7 @@ import com.returdev.animemanga.data.remote.repository.AnimeRemoteRepository
 import com.returdev.animemanga.data.remote.repository.MangaRemoteRepository
 import com.returdev.animemanga.data.remote.service.ApiService
 import com.returdev.animemanga.data.repository.AnimeRepository
+import com.returdev.animemanga.data.repository.LibraryRepository
 import com.returdev.animemanga.data.repository.MangaRepository
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.DefaultRequest
@@ -21,8 +28,10 @@ import io.ktor.http.ContentType
 import io.ktor.http.URLProtocol
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.serialization.json.Json
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.factoryOf
@@ -59,6 +68,38 @@ val datamodule = module {
     cacheDaoFactory()
     dataSourceFactory()
 
+    single {
+        val job = SupervisorJob()
+        val dispatcher = get<CoroutineDispatcher>()
+        CoroutineScope(dispatcher + job)
+    }
+
+    factory<LibraryAnimeDataSource> {
+        LibraryAnimeDataSource(
+            animeDAO = get(),
+            transactionRunner = get<LibraryDatabase>()::transactionRunner
+        )
+    }
+
+    factory<LibraryMangaDataSource> {
+        LibraryMangaDataSource(
+            mangaDAO = get(),
+            transactionRunner = get<LibraryDatabase>()::transactionRunner
+        )
+    }
+
+
+    factoryOf(::LibraryRepository)
+    factory {
+        val libraryDatabase = get<LibraryDatabase>()
+        libraryDatabase.libraryAnimeDao()
+    }
+    factory {
+        val libraryDatabase = get<LibraryDatabase>()
+        libraryDatabase.libraryMangaDao()
+    }
+
+
 }
 
 private fun Module.cacheDaoFactory(){
@@ -74,4 +115,13 @@ private fun Module.dataSourceFactory(){
     factoryOf(::MangaGenreCacheDataSource)
     factoryOf(::SeasonCacheDataSource)
     factoryOf(::CacheMetadataDataSource)
+}
+
+
+private suspend fun RoomDatabase.transactionRunner(transaction : suspend () -> Unit) {
+    this.useReaderConnection {
+        it.withTransaction(Transactor.SQLiteTransactionType.IMMEDIATE) {
+            return@withTransaction transaction()
+        }
+    }
 }
